a584090a497c6bc81e8316ce2c9e034911013bb7
Amanda Mendes <amandamendes@Amandas-MacBook-Air.local>
2023-05-09 21:33:58 -0300

    subindo fix de cabecalhos

--- Exercicios/Ex11.java
+++ Exercicios/Ex11.java
@@ -1,11 +1,11 @@
-
-
-package Exercicios;
 /* Nome do Aluno: Amanda Mendes Santos
  * RA:820141459
  * Nome do Programa:Faça um programa que imprima todos os números pares de 1 a 100 na ordem inversa
  * Data: 09/05/2023
  */
+
+package Exercicios;
+
 public class Ex11 {
     public static void main(String[] args) {
         for (int i = 100; i >= 2; i -= 2) {
